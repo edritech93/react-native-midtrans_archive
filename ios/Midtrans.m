a7a0d0ba162f96e19f0edcfe145c6b6ba0f0688b
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-08-25 13:44:38 +0700

    add new workspace and Podfile

--- ios/Midtrans.m
+++ ios/Midtrans.m
@@ -1,8 +1,17 @@
+#import <Foundation.h>
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(Midtrans, NSObject)
 
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getConstants:
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(checkout:(NSObject)data
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
--- ios/Midtrans.swift
+++ ios/Midtrans.swift
@@ -1,8 +1,32 @@
+import Foundation
+import MidtransKit
+
 @objc(Midtrans)
 class Midtrans: NSObject {
-
+    
     @objc(multiply:withB:withResolver:withRejecter:)
     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
         resolve(a*b)
+    }
+    
+    @objc(getConstants:withRejecter:)
+    func getConstants(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve("123qwe")
+    }
+    
+    @objc(checkout:withResolver:withRejecter:)
+    func checkout(data: NSObject, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        MidtransMerchantClient.shared().requestTransactionToken(
+            withTransactionDetails: transactionDetail,
+            itemDetails: itemDetails,
+            customerDetails: customerDetail) { [self] token, error in
+            if let token = token {
+                let vc = MidtransUIPaymentViewController(token: token)
+                present(vc, animated: true)
+            } else {
+                // do something on error
+            }
+        }
+        resolve(data)
     }
 }
