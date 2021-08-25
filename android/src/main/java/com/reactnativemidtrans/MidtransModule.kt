4ff874ea0b01acf6f7a1a32d02a631589e7c1dfd
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-08-25 11:16:09 +0700

    add midtrans SDK

--- android/src/main/java/com/reactnativemidtrans/MidtransModule.kt
+++ android/src/main/java/com/reactnativemidtrans/MidtransModule.kt
@@ -1,24 +1,122 @@
 package com.reactnativemidtrans
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import android.util.Log
+import com.facebook.react.bridge.*
+import com.midtrans.sdk.corekit.core.MidtransSDK
+import com.midtrans.sdk.corekit.core.PaymentMethod
+import com.midtrans.sdk.corekit.core.TransactionRequest
+import com.midtrans.sdk.corekit.models.ItemDetails
+import com.midtrans.sdk.uikit.SdkUIFlowBuilder
+import java.util.*
 
-class MidtransModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class MidtransModule(reactContext: ReactApplicationContext) :
+  ReactContextBaseJavaModule(reactContext) {
 
-    override fun getName(): String {
-        return "Midtrans"
+  override fun getName(): String {
+    return "Midtrans"
+  }
+
+  override fun getConstants(): MutableMap<String, Any> {
+    val constants: MutableMap<String, Any> = HashMap()
+    val paymentMethod = Arguments.createMap()
+
+    for (i in PaymentMethod.values().indices) {
+      paymentMethod.putInt(PaymentMethod.values()[i].name, i)
     }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
+    constants["PAYMENT_METHOD"] = paymentMethod
+
+    return constants
+  }
+
+  @ReactMethod
+  fun checkout(options: ReadableMap, promise: Promise) {
+    SdkUIFlowBuilder.init()
+      .setClientKey(options.getString("client_key"))
+      .setContext(reactApplicationContext)
+      .setTransactionFinishedCallback {
+
+        if (it.response != null) {
+          Log.d("REACT_NATIVE_MIDTRANS", it.response.toString())
+        }
+
+        val result = Arguments.createMap()
+
+        result.putBoolean("is_transaction_canceled", it.isTransactionCanceled)
+
+        if (it.source != null) {
+          result.putString("source", it.source)
+        }
+
+        if (it.status != null) {
+          result.putString("status", it.status)
+        }
+
+        if (it.statusMessage != null) {
+          result.putString("status_message", it.statusMessage)
+        }
+
+        if (it.isTransactionCanceled) {
+          promise.reject("transaction_canceled", "transaction_canceled")
+          return@setTransactionFinishedCallback
+        }
+
+        if (it.status == "success") {
+          promise.resolve(it.status)
+        } else {
+          promise.reject(it.status, it.statusMessage)
+        }
+      }
+      .setMerchantBaseUrl(options.getString("merchant_base_url"))
+      .enableLog(true)
+      .setLanguage(options.getString("language"))
+      .buildSDK()
+
+    val setting = MidtransSDK.getInstance().uiKitCustomSetting
+    setting.isSkipCustomerDetailsPages = true
+    MidtransSDK.getInstance().uiKitCustomSetting = setting
+
+    val items = options.getArray("items")
+
+    var totalPrice = 0.0
+
+    val itemDetailsList: ArrayList<ItemDetails> = ArrayList<ItemDetails>()
+
+    val size = items?.size() ?: 0
+
+    for (i in 0 until size) {
+      items?.getMap(i).let {
+        val price = it?.getDouble("price") ?: 0.0
+        val quality = it?.getDouble("quantity")?.toInt() ?: 0
+        val itemDetails = ItemDetails(
+          it?.getString("id"),
+          price,
+          quality,
+          it?.getString("name")
+        )
+
+        totalPrice += (price * quality)
+
+        itemDetailsList.add(itemDetails)
+      }
     }
 
-    
+
+    val transactionRequest = TransactionRequest(options.getString("order_id") ?: "", totalPrice)
+
+    transactionRequest.itemDetails = itemDetailsList
+
+    MidtransSDK.getInstance().transactionRequest = transactionRequest
+
+    val selectedPaymentMethod = options.getDouble("payment_method") ?: -1.0
+
+    if (selectedPaymentMethod == -1.0) {
+      MidtransSDK.getInstance().startPaymentUiFlow(currentActivity)
+    } else {
+      MidtransSDK.getInstance()
+        .startPaymentUiFlow(currentActivity, PaymentMethod.values()[selectedPaymentMethod.toInt()]);
+    }
+  }
+
+
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,38 @@
 import { NativeModules } from 'react-native';
 
-type MidtransType = {
-  multiply(a: number, b: number): Promise<number>;
-};
+interface CheckoutItem {
+  id: string;
+  price: number;
+  quantity: number;
+  name: string;
+}
+
+interface CheckoutOptions {
+  client_key: string;
+  merchant_base_url: string;
+  language?: string;
+  items: CheckoutItem[];
+  order_id: string;
+  payment_method?: number;
+}
 
 const { Midtrans } = NativeModules;
 
-export default Midtrans as MidtransType;
+const exported = {
+  checkout: (options: CheckoutOptions): Promise<any> => {
+    if (!options.payment_method) {
+      options.payment_method = -1;
+    }
+
+    if (!options.language) {
+      options.language = 'en';
+    }
+
+    return Midtrans.checkout(options);
+  },
+  getConstants: () => {
+    return Midtrans.getConstants();
+  },
+};
+
+export default exported;
